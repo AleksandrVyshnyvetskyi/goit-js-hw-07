a2b9b8ea7d01fec9257067d2ab791f445c41301c
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-08-01 22:15:53 +0300

    start

---
+++ js/01-gallery.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.    +
+// Реализация делегирования на div.gallery и получение url большого изображения.
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и 
+// добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку 
+// модального окна с изображением из примеров библиотеки basicLightbox.
+
+
+const gallaryContainer = document.querySelector('.gallery');
+const imgMarkup = createGallleryMarkup(galleryItems);
+
+gallaryContainer.insertAdjacentHTML('beforeend', imgMarkup)
+
+console.log(createGallleryMarkup(galleryItems))
+
+function createGallleryMarkup(galleryItems){
+    return galleryItems.map(({ preview, original, description }) => { 
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                        />
+                    </a>
+                </div>`;
+    }).join('');
+};
+
